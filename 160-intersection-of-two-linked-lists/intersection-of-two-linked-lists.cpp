1a8ef68eef0ce37b4d5ab4159895b19e356e18ed
not-thanigai <121597035+not-thanigai@users.noreply.github.com>
2024-02-18 15:11:52 +0530

    Time: 40 ms (54.93%) | Memory: 17 MB (46.13%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -12,15 +12,13 @@ public:
         ListNode *ptr1 = headA;
         ListNode *ptr2 = headB;
         while(ptr1!=ptr2){
+            ptr1=ptr1->next;
             ptr2=ptr2->next;
-            if(ptr2==NULL){
-                ptr2=headB;
-                ptr1=ptr1->next;
-                if(ptr1==NULL){
-                    return 0;
-                }
-            }
+            if(ptr1==NULL && ptr2 ==NULL) return 0;
+
+            if(ptr1==NULL) ptr1 = headB;
+            if(ptr2==NULL) ptr2 = headA;
         }
-        return ptr2;
+        return ptr1;
     }
 };
